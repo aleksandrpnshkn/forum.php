80d69a4afba36bcc3b0ebf2974551691c6a57dbc
aleksandr <aleksandrkrzhn@yandex.ru>
2021-01-05 21:19:48 +0400

    Use 255 chars for password as php.net recommends

--- database/migrate.sql
+++ database/migrate.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL, # for bcrypt 60 would be enough, but default php algorithm can change in future
     avatar_path VARCHAR(255),
     messages_count INT UNSIGNED NOT NULL DEFAULT 0, # counting every time amount of messages is heavy task
 
