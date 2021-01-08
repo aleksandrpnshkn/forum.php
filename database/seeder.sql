bcd91c2d7eecf4b7ba1c4c1ba09d1a3c668bd11b
aleksandr <aleksandrkrzhn@yandex.ru>
2021-01-08 22:38:17 +0300

    Add data for seeding boards and threads

--- database/seeder.sql
+++ database/seeder.sql
@@ -10,3 +10,37 @@ VALUES
 
 INSERT INTO categories (name)
 VALUES ('Vegetables'), ('Coins'), ('Loud noises');
+
+INSERT INTO boards (name, slug, description, category_id, author_id)
+VALUES
+    ('Quinible', 'quinible', 'He dreamed of eating green apples with worms.', 1, 1),
+    ('Discountenance', 'discountenance', NULL, 1, 2),
+    ('Vocative', 'vocative', NULL, 1, 2),
+    ('Zufolo', 'zufolo', 'small flute used to train songbirds', 2, 1),
+    ('Theocentrism', 'theocentrism', NULL, 2, 1),
+    ('Bibliopoesy', 'bibliopoesy', 'The hummingbird\'s wings blurred while it eagerly sipped the sugar water from the feeder.', 3, 1);
+
+INSERT INTO threads (name, status, is_pinned, board_id, author_id)
+VALUES
+    ('If my calculator had a history', 'open', true, 1, 1),
+    ('I love eating toasted cheese and tuna sandwiches', 'open', true, 1, 2),
+    ('We need to rent a room for our party', 'open', false, 1, 2),
+    ('his seven-layer cake only had six layers', 'open', false, 2, 1),
+    ('If my calculator had a history', 'open', true, 1, 1),
+    ('I love eating toasted cheese and tuna sandwiches', 'open', true, 1, 2),
+    ('We need to rent a room for our party', 'open', false, 1, 2),
+    ('his seven-layer cake only had six layers', 'open', false, 2, 1),
+    ('If my calculator had a history', 'open', true, 1, 1),
+    ('I love eating toasted cheese and tuna sandwiches', 'open', true, 1, 2),
+    ('We need to rent a room for our party', 'open', false, 1, 2),
+    ('his seven-layer cake only had six layers', 'open', false, 2, 1),
+    ('If my calculator had a history', 'open', true, 1, 1),
+    ('I love eating toasted cheese and tuna sandwiches', 'open', true, 1, 2),
+    ('We need to rent a room for our party', 'open', false, 1, 2),
+    ('his seven-layer cake only had six layers', 'open', false, 2, 1),
+    ('If you like tuna and tomato sauce- try combining the two', 'open', false, 2, 1),
+    ('If I don’t like something, I’ll stay away from it.', 'open', false, 3, 1),
+    ('She had convinced her kids that any mushroom found on the ground would kill them if they touched it.', 'open', false, 4, 1),
+    ('The beach was crowded with snow leopards.', 'open', false, 5, 1),
+    ('Nudist colonies shun fig-leaf couture', 'open', false, 6, 1),
+    ('He embraced his new life as an eggplant', 'open', false, 6, 1);
